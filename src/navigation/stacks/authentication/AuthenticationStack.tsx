ce588898ab5fd1151c19c7dcb13ab09161013315
a-simplecode <amineamine19961996@gmail.com>
2023-01-26 16:22:48 +0200

    FIX:

--- src/navigation/stacks/authentication/AuthenticationStack.tsx
+++ src/navigation/stacks/authentication/AuthenticationStack.tsx
@@ -2,19 +2,13 @@ import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthenticationStackNavigatorParamList } from "./AuthenticationStack.types";
 import Login from "features/authentication/login/Login";
-import ForgotPassword from "features/authentication/forgotPassword/ForgotPassword";
 
 const AuthenticationStack = createNativeStackNavigator<AuthenticationStackNavigatorParamList>();
 
 const AuthenticationStackNavigator: React.FC = () => {
   return (
     <AuthenticationStack.Navigator>
-      <AuthenticationStack.Screen name="login" component={Login} options={{ headerTitle: "Screen title" }} />
-      <AuthenticationStack.Screen
-        name="forgotPassword"
-        component={ForgotPassword}
-        options={{ headerTitle: "forgot password" }}
-      />
+      <AuthenticationStack.Screen name="login" component={Login} options={{ headerTitle: "Login" }} />
     </AuthenticationStack.Navigator>
   );
 };
