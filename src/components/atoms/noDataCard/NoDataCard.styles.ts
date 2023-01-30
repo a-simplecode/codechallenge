a1174ce4b077641c4307f01cc4bcc62a93c4135a
a-simplecode <amineamine19961996@gmail.com>
2023-01-30 12:03:45 +0200

    FIX: no data found

--- src/components/atoms/noDataCard/NoDataCard.styles.ts
+++ src/components/atoms/noDataCard/NoDataCard.styles.ts
@@ -2,12 +2,13 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   view: {
-    height: "100%",
-    display: "flex",
-    justifyContent: "center",
+    height: "87%",
     alignItems: "center",
+    justifyContent: "center",
   },
   text: {
+    width: "95%",
+    textAlign: "center",
     fontSize: 30,
     paddingVertical: 30,
     paddingHorizontal: 60,
